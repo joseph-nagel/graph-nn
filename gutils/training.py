3a3aeafc9570e6b013eba1d4616658b7b033b948
Joseph Nagel <JosephBNagel@gmail.com>
2024-07-01 14:25:31 +0200

    Extend training loop

--- gutils/training.py
+++ gutils/training.py
@@ -4,25 +4,6 @@ import torch
 
 
 @torch.no_grad()
-def test_loss(model,
-              criterion,
-              data,
-              mask=None):
-    '''Compute loss.'''
-
-    model.eval()
-
-    y_pred = model(data.x, data.edge_index)
-
-    if mask is None:
-        loss = criterion(y_pred, data.y)
-    else:
-        loss = criterion(y_pred[mask], data.y[mask])
-
-    return loss
-
-
-@torch.no_grad()
 def accuracy(preds, targets):
     '''Calculate accuracy.'''
     return (preds == targets).sum() / len(preds)
@@ -33,13 +14,28 @@ def train(model,
           optimizer,
           num_epochs,
           data,
-          mask=None,
           log_every=1):
     '''Train model.'''
 
-    # val_loss = test_loss(data, model, criterion)
-    # print('Before training, val. loss: {:.2e}'.format(val_loss))
+    # compute val. loss/acc.
+    model.eval()
 
+    with torch.no_grad():
+        y_pred = model(data.x, data.edge_index)
+
+        val_loss = criterion(
+            y_pred[data.val_mask],
+            data.y[data.val_mask]
+        )
+
+        val_acc = accuracy(
+            y_pred[data.val_mask].argmax(dim=1),
+            data.y[data.val_mask]
+        )
+
+    print(f'Before training, val. loss: {val_loss:.2e}, val. acc.: {val_acc:.2f}')
+
+    # loop over epochs
     for epoch_idx in range(num_epochs):
 
         # perform train step
@@ -49,10 +45,10 @@ def train(model,
 
         y_pred = model(data.x, data.edge_index)
 
-        if mask is None:
-            loss = criterion(y_pred, data.y)
-        else:
-            loss = criterion(y_pred[mask], data.y[mask])
+        loss = criterion(
+            y_pred[data.train_mask],
+            data.y[data.train_mask]
+        )
 
         loss.backward()
         optimizer.step()
@@ -60,16 +56,31 @@ def train(model,
         # monitor performance
         if (epoch_idx + 1) % log_every == 0:
 
-            # compute val. error
-            # val_loss = test_loss(data, model, criterion)
+            model.eval()
 
-            # calculate accuracy
-            if mask is None:
-                acc = accuracy(y_pred.argmax(dim=1), data.y)
-            else:
-                acc = accuracy(y_pred[mask].argmax(dim=1), data.y[mask])
+            with torch.no_grad():
+                train_acc = accuracy(
+                    y_pred[data.train_mask].argmax(dim=1),
+                    data.y[data.train_mask]
+                )
+
+                y_pred = model(data.x, data.edge_index) # preds may differ in eval mode
+
+                val_loss = criterion(
+                    y_pred[data.val_mask],
+                    data.y[data.val_mask]
+                )
+
+                val_acc = accuracy(
+                    y_pred[data.val_mask].argmax(dim=1),
+                    data.y[data.val_mask]
+                )
 
             # print summary
-            print('Epoch: {:d}, loss: {:.2e}, acc.: {:.2f}'. \
-                  format(epoch_idx + 1, loss.detach().item(), acc.detach().item()))
+            print(
+                'Epoch: {:d}, loss: {:.2e}, acc.: {:.2f}, val. loss: {:.2e}, val. acc.: {:.2f}'.format(
+                    epoch_idx + 1, loss.detach().item(), train_acc.detach().item(),
+                    val_loss.detach().item(), val_acc.detach().item()
+                )
+            )
 
